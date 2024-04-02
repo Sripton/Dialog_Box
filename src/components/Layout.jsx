1992de04fc0daa81e2b39928f4acaf7a41889c13
Elmar Erzikhanov <elmarerzikhanov@MacBook-Pro-Elmar.local>
2024-04-02 20:23:55 +0300

    Стилизация меню

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -7,6 +7,9 @@ export default function Layout({ initState }) {
       <head>
         <meta charSet="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="stylesheet" href="/css/normalize.css" />
+        <link rel="stylesheet" href="/css/reset.css" />
+        <link rel="stylesheet" href="/css/style.css" />
         <script defer src="./app.js" />
         <script defer src="./vendor.js" />
         <script
--- src/components/pages/Navbar.jsx
+++ src/components/pages/Navbar.jsx
@@ -1,5 +1,38 @@
 import React from "react";
 
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <div className="header">
+      <div className="header-logo">
+        <img src="/header/logo.png" alt="logotip" />
+      </div>
+      <div className="header-nav">
+        <ul>
+          <li>
+            <a href="/">О нас</a>
+          </li>
+          <li>
+            <a href="/">Контакты</a>
+          </li>
+          <li>
+            <a href="/">Регистрация</a>
+          </li>
+          <li>
+            <a href="/">Вход</a>
+          </li>
+        </ul>
+      </div>
+      <div className="header-icon">
+        <a href="/facebook">
+          <img src="/header/Facebook.png" alt="iconFacebook" />
+        </a>
+        <a href="/lindkedin">
+          <img src="header/Linkedin.png" alt="iconLinkedin" />
+        </a>
+        <a href="/twitter">
+          <img src="header/Twitter.png" alt="iconTwitter" />
+        </a>
+      </div>
+    </div>
+  );
 }
